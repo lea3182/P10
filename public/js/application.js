f113b4e0a9bf9132587f99d8795301bd76f49bd0
Leandra Kim <lkim3182@gmail.com>
2015-04-23 16:24:35 -0700

    Display Google Maps

--- public/js/application.js
+++ public/js/application.js
@@ -1,8 +1,20 @@
-$(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
+// $(document).ready(function() {
+//   // This is called after the document has loaded in its entirety
+//   // This guarantees that any elements we bind to will exist on the page
+//   // when we try to bind to them
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+//   // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
 
-});
+// });
+
+function initialize() {
+
+  var mapOptions = {
+    center: { lat: 37.787508, lng: -122.403769},
+    zoom: 13
+  };
+
+  var map = new google.maps.Map(document.getElementById('map-canvas'),mapOptions);
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
